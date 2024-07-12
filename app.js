259d1d5551b46dc64cabadd96dccb38059f219b1
namansinghal <72150803+namansinghal111@users.noreply.github.com>
2024-07-12 23:35:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 //! Connect to mongodb
 mongoose
-  .connect("mongodb://localhost:27017/prodauth")
+  .connect("mongodb+srv://prodapi:prodapi@prodapi.6zxrkp9.mongodb.net/")
   .then(() => console.log("Db connected successfully"))
   .catch((e) => console.log(e));
 
